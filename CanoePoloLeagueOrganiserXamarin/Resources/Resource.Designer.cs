66a3fbfd681690aa81298b41becee6b2b50c4dd6
CeddlyBurge <CeddlyBurge@gmail.com>
2016-09-15 08:08:19 +0100

    Make the buttons smaller

--- CanoePoloLeagueOrganiserXamarin/Resources/Resource.Designer.cs
+++ CanoePoloLeagueOrganiserXamarin/Resources/Resource.Designer.cs
@@ -60,41 +60,41 @@ namespace CanoePoloLeagueOrganiserXamarin
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050011
-			public const int Add = 2131034129;
+			// aapt resource value: 0x7f05000f
+			public const int Add = 2131034127;
 			
 			// aapt resource value: 0x7f050006
 			public const int AwayTeam = 2131034118;
 			
-			// aapt resource value: 0x7f05000b
-			public const int Down = 2131034123;
+			// aapt resource value: 0x7f050009
+			public const int Down = 2131034121;
 			
-			// aapt resource value: 0x7f050015
-			public const int GameOrder = 2131034133;
+			// aapt resource value: 0x7f050013
+			public const int GameOrder = 2131034131;
 			
-			// aapt resource value: 0x7f050012
-			public const int Games = 2131034130;
+			// aapt resource value: 0x7f050010
+			public const int Games = 2131034128;
 			
 			// aapt resource value: 0x7f050002
 			public const int HomeTeam = 2131034114;
 			
-			// aapt resource value: 0x7f050013
-			public const int Matches = 2131034131;
+			// aapt resource value: 0x7f050011
+			public const int Matches = 2131034129;
 			
-			// aapt resource value: 0x7f05000f
-			public const int OptimisationExplanation = 2131034127;
+			// aapt resource value: 0x7f05000c
+			public const int OptimisationExplanation = 2131034124;
 			
-			// aapt resource value: 0x7f05000e
-			public const int Optimise = 2131034126;
+			// aapt resource value: 0x7f05000b
+			public const int Optimise = 2131034123;
 			
-			// aapt resource value: 0x7f050014
-			public const int OptimiseGameOrder = 2131034132;
+			// aapt resource value: 0x7f050012
+			public const int OptimiseGameOrder = 2131034130;
 			
-			// aapt resource value: 0x7f05000d
-			public const int Remove = 2131034125;
+			// aapt resource value: 0x7f05000a
+			public const int Remove = 2131034122;
 			
-			// aapt resource value: 0x7f050009
-			public const int Up = 2131034121;
+			// aapt resource value: 0x7f050008
+			public const int Up = 2131034120;
 			
 			// aapt resource value: 0x7f050004
 			public const int V = 2131034116;
@@ -111,20 +111,14 @@ namespace CanoePoloLeagueOrganiserXamarin
 			// aapt resource value: 0x7f050005
 			public const int relativeLayout3 = 2131034117;
 			
-			// aapt resource value: 0x7f05000c
-			public const int relativeLayout4 = 2131034124;
-			
-			// aapt resource value: 0x7f050008
-			public const int relativeLayout5 = 2131034120;
-			
-			// aapt resource value: 0x7f05000a
-			public const int relativeLayout6 = 2131034122;
+			// aapt resource value: 0x7f05000e
+			public const int relativeLayout4 = 2131034126;
 			
 			// aapt resource value: 0x7f050007
 			public const int relativeLayout7 = 2131034119;
 			
-			// aapt resource value: 0x7f050010
-			public const int textView2 = 2131034128;
+			// aapt resource value: 0x7f05000d
+			public const int textView2 = 2131034125;
 			
 			static Id()
 			{
