97c4e8abf4acbf9cfc07cd3d2d3e9becd3287c7e
CeddlyBurge <CeddlyBurge@gmail.com>
2016-09-21 08:10:07 +0100

    Introduce new interface and class to make things omore iocable

---
+++ CanoePoloLeagueOrganiser/GameOrderCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanoePoloLeagueOrganiser
+{
+    public class GameOrderCalculator : IGameOrderCalculator
+    {
+        readonly IPragmatiser pragmatiser;
+
+        public GameOrderCalculator(IPragmatiser pragmatiser)
+        {
+            Contract.Requires(pragmatiser != null);
+
+            this.pragmatiser = pragmatiser;
+        }
+
+        public GameOrderCandidate CalculateOriginalGameOrder(IReadOnlyList<Game> games)
+        {
+            return new TournamentDayCalculator(games, pragmatiser).CalculateOriginalGameOrder();
+        }
+
+        public GameOrderCalculation OptimiseGameOrder(IReadOnlyList<Game> games)
+        {
+            return new TournamentDayCalculator(games, pragmatiser).CalculateGameOrder();
+        }
+    }
+}
---
+++ CanoePoloLeagueOrganiser/IGameOrderCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanoePoloLeagueOrganiser
+{
+    public interface IGameOrderCalculator
+    {
+        GameOrderCandidate CalculateOriginalGameOrder(IReadOnlyList<Game> games);
+        GameOrderCalculation OptimiseGameOrder(IReadOnlyList<Game> games);
+    }
+}
