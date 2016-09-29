2b8748c2b04c7c619ae02fa6eb1ce80b5950ee33
CeddlyBurge <CeddlyBurge@gmail.com>
2016-09-29 08:20:13 +0100

    Add icons and screenshots for google play

--- CanoePoloLeagueOrganiserXamarin/Resources/Resource.Designer.cs
+++ CanoePoloLeagueOrganiserXamarin/Resources/Resource.Designer.cs
@@ -45,7 +45,7 @@ namespace CanoePoloLeagueOrganiserXamarin
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int canoe_polo_ball = 2130837504;
+			public const int ic_canoe_polo_ball = 2130837504;
 			
 			// aapt resource value: 0x7f020001
 			public const int Icon = 2130837505;
@@ -63,65 +63,65 @@ namespace CanoePoloLeagueOrganiserXamarin
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f05000f
-			public const int Add = 2131034127;
+			// aapt resource value: 0x7f06000f
+			public const int Add = 2131099663;
 			
-			// aapt resource value: 0x7f050006
-			public const int AwayTeam = 2131034118;
+			// aapt resource value: 0x7f060006
+			public const int AwayTeam = 2131099654;
 			
-			// aapt resource value: 0x7f050009
-			public const int Down = 2131034121;
+			// aapt resource value: 0x7f060009
+			public const int Down = 2131099657;
 			
-			// aapt resource value: 0x7f050013
-			public const int GameOrder = 2131034131;
+			// aapt resource value: 0x7f060013
+			public const int GameOrder = 2131099667;
 			
-			// aapt resource value: 0x7f050010
-			public const int Games = 2131034128;
+			// aapt resource value: 0x7f060010
+			public const int Games = 2131099664;
 			
-			// aapt resource value: 0x7f05000c
-			public const int Help = 2131034124;
+			// aapt resource value: 0x7f06000c
+			public const int Help = 2131099660;
 			
-			// aapt resource value: 0x7f050002
-			public const int HomeTeam = 2131034114;
+			// aapt resource value: 0x7f060002
+			public const int HomeTeam = 2131099650;
 			
-			// aapt resource value: 0x7f050011
-			public const int Matches = 2131034129;
+			// aapt resource value: 0x7f060011
+			public const int Matches = 2131099665;
 			
-			// aapt resource value: 0x7f05000b
-			public const int Optimise = 2131034123;
+			// aapt resource value: 0x7f06000b
+			public const int Optimise = 2131099659;
 			
-			// aapt resource value: 0x7f050012
-			public const int OptimiseGameOrder = 2131034130;
+			// aapt resource value: 0x7f060012
+			public const int OptimiseGameOrder = 2131099666;
 			
-			// aapt resource value: 0x7f05000a
-			public const int Remove = 2131034122;
+			// aapt resource value: 0x7f06000a
+			public const int Remove = 2131099658;
 			
-			// aapt resource value: 0x7f050008
-			public const int Up = 2131034120;
+			// aapt resource value: 0x7f060008
+			public const int Up = 2131099656;
 			
-			// aapt resource value: 0x7f050004
-			public const int V = 2131034116;
+			// aapt resource value: 0x7f060004
+			public const int V = 2131099652;
 			
-			// aapt resource value: 0x7f050000
-			public const int linearLayout1 = 2131034112;
+			// aapt resource value: 0x7f060000
+			public const int linearLayout1 = 2131099648;
 			
-			// aapt resource value: 0x7f050001
-			public const int relativeLayout1 = 2131034113;
+			// aapt resource value: 0x7f060001
+			public const int relativeLayout1 = 2131099649;
 			
-			// aapt resource value: 0x7f050003
-			public const int relativeLayout2 = 2131034115;
+			// aapt resource value: 0x7f060003
+			public const int relativeLayout2 = 2131099651;
 			
-			// aapt resource value: 0x7f050005
-			public const int relativeLayout3 = 2131034117;
+			// aapt resource value: 0x7f060005
+			public const int relativeLayout3 = 2131099653;
 			
-			// aapt resource value: 0x7f05000e
-			public const int relativeLayout4 = 2131034126;
+			// aapt resource value: 0x7f06000e
+			public const int relativeLayout4 = 2131099662;
 			
-			// aapt resource value: 0x7f050007
-			public const int relativeLayout7 = 2131034119;
+			// aapt resource value: 0x7f060007
+			public const int relativeLayout7 = 2131099655;
 			
-			// aapt resource value: 0x7f05000d
-			public const int textView2 = 2131034125;
+			// aapt resource value: 0x7f06000d
+			public const int textView2 = 2131099661;
 			
 			static Id()
 			{
@@ -136,14 +136,14 @@ namespace CanoePoloLeagueOrganiserXamarin
 		public partial class Layout
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int GameRow = 2130903040;
+			// aapt resource value: 0x7f040000
+			public const int GameRow = 2130968576;
 			
-			// aapt resource value: 0x7f030001
-			public const int Games = 2130903041;
+			// aapt resource value: 0x7f040001
+			public const int Games = 2130968577;
 			
-			// aapt resource value: 0x7f030002
-			public const int Main = 2130903042;
+			// aapt resource value: 0x7f040002
+			public const int Main = 2130968578;
 			
 			static Layout()
 			{
@@ -155,14 +155,30 @@ namespace CanoePoloLeagueOrganiserXamarin
 			}
 		}
 		
+		public partial class Mipmap
+		{
+			
+			// aapt resource value: 0x7f030000
+			public const int ic_launcher = 2130903040;
+			
+			static Mipmap()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Mipmap()
+			{
+			}
+		}
+		
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int ApplicationName = 2130968577;
+			// aapt resource value: 0x7f050001
+			public const int ApplicationName = 2131034113;
 			
-			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
+			// aapt resource value: 0x7f050000
+			public const int Hello = 2131034112;
 			
 			static String()
 			{
