b5232e37251a36e856be67ee709388be7bae1cfa
CeddlyBurge <ceddlyburge@gmail.com>
2017-10-18 15:33:34 +0100

    Refactor GameOrderCalculation

--- CanoePoloLeagueOrganiser/GameOrderCalculation.cs
+++ CanoePoloLeagueOrganiser/GameOrderCalculation.cs
@@ -2,23 +2,16 @@
 
 namespace CanoePoloLeagueOrganiser
 {
-    public class GameOrderPossiblyNullCalculation
-    {
-        public GameOrderCandidate OptimisedGameOrder { get; }
-        public PragmatisationLevel PragmatisationLevel { get; }
-        public string OptimisationMessage { get; }
-
-        public GameOrderPossiblyNullCalculation(GameOrderCandidate optimisedGameOrder, PragmatisationLevel pragmatisationLevel, string optimisationMessage)
-        {
-            OptimisationMessage = optimisationMessage;
-            PragmatisationLevel = pragmatisationLevel;
-            OptimisedGameOrder = optimisedGameOrder;
-        }
-    }
-
     public class GameOrderCalculation : GameOrderPossiblyNullCalculation
     {
-        public GameOrderCalculation(GameOrderCandidate optimisedGameOrder, PragmatisationLevel pragmatisationLevel, string optimisationMessage) : base(optimisedGameOrder, pragmatisationLevel, optimisationMessage)
+        public GameOrderCalculation(
+            GameOrderCandidate optimisedGameOrder, 
+            PragmatisationLevel pragmatisationLevel, 
+            string optimisationMessage) : 
+            base(
+                optimisedGameOrder, 
+                pragmatisationLevel, 
+                optimisationMessage)
         {
             Requires(optimisedGameOrder != null);
         }
